0a8e26fcf80c16f8ea09bcde8dcea9767f2075c1
peter.ljunglof@heatherleaf.se <peter.ljunglof@heatherleaf.se@7ef4e256-bf0e-11de-afc4-699a8c260df9>
2010-02-02 16:16:16 +0000

    added 2010 to copyright notice

--- srgs.js
+++ srgs.js
@@ -1,6 +1,6 @@
 //
 //  srgs.js
-//  Copyright (C) 2009, Peter Ljunglöf. All rights reserved.
+//  Copyright (C) 2009, 2010, Peter Ljunglöf. All rights reserved.
 //
 /*
   This program is free software: you can redistribute it and/or modify
