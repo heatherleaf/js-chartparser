dadc7913ae6657f5747d68216ab7f188eb12b60e
peter.ljunglof@heatherleaf.se <peter.ljunglof@heatherleaf.se@7ef4e256-bf0e-11de-afc4-699a8c260df9>
2010-02-02 16:43:18 +0000

    changed indentation level (no code change)

--- runparser.js
+++ runparser.js
@@ -10,85 +10,86 @@
 // helper functions for the parser demo
 
 function getElement(id) {
-  return document.getElementById(id);
+    return document.getElementById(id);
 }
 
 function appendElement(parent, element, text) {
-  var elem = document.createElement(element);
-  if (text)
-    elem.appendChild(document.createTextNode(text));
-  parent.appendChild(elem);
-  return elem
+    var elem = document.createElement(element);
+    if (text) {
+        elem.appendChild(document.createTextNode(text));
+    }
+    parent.appendChild(elem);
+    return elem
 }
 
 function treeString(tree) {
-  if (typeof(tree) == 'object' && tree.label) {
-    str = "(" + tree.label
-    if (tree.children) {
-      for (var i in tree.children) {
-        str += " " + treeString(tree.children[i])
-      }
+    if (typeof(tree) == 'object' && tree.label) {
+        str = "(" + tree.label
+        if (tree.children) {
+            for (var i in tree.children) {
+                str += " " + treeString(tree.children[i])
+            }
+        }
+        return str + ")"
+    } else {
+        return stringRepr(tree)
     }
-    return str + ")"
-  } else {
-    return stringRepr(tree)
-  }
 }
 
 // remember the original makeTree function, since runParser() redefines it:
 var originalMakeTree = makeTree;
 
 function runParser(input) {
-  var resultsDiv = getElement("results");
-  appendElement(resultsDiv, "hr");
-  appendElement(resultsDiv, "h3", '"' + input.join(" ") + '"');
-  
-  if (getElement("parsetrees") && getElement("parsetrees").checked) {
-    makeTree = originalMakeTree;
-    appendElement(resultsDiv, "em", "[building parsetrees] ");
-  } else {
-    makeTree = false;
-  }
-  
-  var maybeFilter;
-  if (getElement("usefilter") && getElement("usefilter").checked) {
-    maybeFilter = filter;
-    appendElement(resultsDiv, "em", "[using left-corner filter] ");
-  }
-  
-  var startTime = new Date();
-  var parseChart = parse(input, grammar, grammar.$root, maybeFilter);
-  var parseTime = new Date() - startTime;
-  
-  var statistics = parseChart.statistics()
-  appendElement(resultsDiv, "p", "Chart size: " + statistics.nrEdges + " edges" +
-                " (" + statistics.nrPassiveEdges + " passive)");
-  appendElement(resultsDiv, "p", "Parse time: " + parseTime + " ms" + 
-                " (" + (parseTime / statistics.nrEdges).toFixed(2) + " ms/edge)");
-  
-  var parseTrees = parseChart.treesForRule(grammar.$root);
-  if (parseTrees) { 
-    appendElement(resultsDiv, "p", "Parse trees: " + parseTrees.length);
-    var resultList = appendElement(resultsDiv, "ol");
-    for (var i in parseTrees) {
-      var tree = parseTrees[i];
-      if (tree.data) {
-        var listItem = appendElement(resultList, "li", stringRepr(tree.data));
-        appendElement(listItem, "p", treeString(tree));
-      } else {
-        appendElement(resultList, "li", treeString(tree));
-      }
+    var resultsDiv = getElement("results");
+    appendElement(resultsDiv, "hr");
+    appendElement(resultsDiv, "h3", '"' + input.join(" ") + '"');
+    
+    if (getElement("parsetrees") && getElement("parsetrees").checked) {
+        makeTree = originalMakeTree;
+        appendElement(resultsDiv, "em", "[building parsetrees] ");
+    } else {
+        makeTree = false;
     }
-  } else {
-    appendElement(resultsDiv, "p", "No results found!");
-  }
+    
+    var maybeFilter;
+    if (getElement("usefilter") && getElement("usefilter").checked) {
+        maybeFilter = filter;
+        appendElement(resultsDiv, "em", "[using left-corner filter] ");
+    }
+    
+    var startTime = new Date();
+    var parseChart = parse(input, grammar, grammar.$root, maybeFilter);
+    var parseTime = new Date() - startTime;
+    
+    var statistics = parseChart.statistics()
+    appendElement(resultsDiv, "p", "Chart size: " + statistics.nrEdges + " edges" +
+                  " (" + statistics.nrPassiveEdges + " passive)");
+    appendElement(resultsDiv, "p", "Parse time: " + parseTime + " ms" + 
+                  " (" + (parseTime / statistics.nrEdges).toFixed(2) + " ms/edge)");
+    
+    var parseTrees = parseChart.treesForRule(grammar.$root);
+    if (parseTrees) { 
+        appendElement(resultsDiv, "p", "Parse trees: " + parseTrees.length);
+        var resultList = appendElement(resultsDiv, "ol");
+        for (var i in parseTrees) {
+            var tree = parseTrees[i];
+            if (tree.data) {
+                var listItem = appendElement(resultList, "li", stringRepr(tree.data));
+                appendElement(listItem, "p", treeString(tree));
+            } else {
+                appendElement(resultList, "li", treeString(tree));
+            }
+        }
+    } else {
+        appendElement(resultsDiv, "p", "No results found!");
+    }
 }
 
 function runWordParser() {
-  runParser(getElement("input").value.split(/\s+/));
+    runParser(getElement("input").value.split(/\s+/));
 }
 
 function runCharacterParser() {
-  runParser(getElement("input").value.split(""));
+    runParser(getElement("input").value.split(""));
 }
 
